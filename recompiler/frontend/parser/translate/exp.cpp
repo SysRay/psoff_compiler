1b40fd904fd1ee570c7f2c632c0326178cd61e9f
SysRay <s.baliet@gmx.de>
2025-09-23 18:00:18 +0200

    +exp dummy

--- recompiler/frontend/parser/translate/exp.cpp
+++ recompiler/frontend/parser/translate/exp.cpp
@@ -1,18 +1,47 @@
+#include "../../frontend.h"
 #include "../debug_strings.h"
 #include "../opcodes_table.h"
 #include "builder.h"
 #include "encodings.h"
+#include "instruction_builder.h"
 #include "translate.h"
 
+#include <bitset>
 #include <format>
 #include <stdexcept>
 
 namespace compiler::frontend::translate {
 bool handleExp(Builder& builder, parser::pc_t pc, parser::code_p_t* pCode) {
+  using namespace parser;
+
   auto inst = EXP(getU64(*pCode));
+
+  std::bitset<4> const enable     = inst.template get<EXP::Field::EN>();
+  uint8_t const        target     = inst.template get<EXP::Field::TGT>();
+  bool const           compressed = inst.template get<EXP::Field::COMPR>();
+  bool const           isLast     = inst.template get<EXP::Field::DONE>();
+
+  auto src0 = OpSrc((eOperandKind)((OperandKind_t)eOperandKind::VGPR + inst.template get<EXP::Field::VSRC0>()));
+  auto src1 = OpSrc((eOperandKind)((OperandKind_t)eOperandKind::VGPR + inst.template get<EXP::Field::VSRC1>()));
+  auto src2 = OpSrc((eOperandKind)((OperandKind_t)eOperandKind::VGPR + inst.template get<EXP::Field::VSRC2>()));
+  auto src3 = OpSrc((eOperandKind)((OperandKind_t)eOperandKind::VGPR + inst.template get<EXP::Field::VSRC2>()));
 
   *pCode += 2;
 
+  if (target >= 0x0 && target <= 0x7) { // Attachments
+    bool const useExecMask = inst.template get<EXP::Field::VM>();
+    if (useExecMask) {
+      builder.createInstruction(create::discardOp(OpSrc(eOperandKind::ExecLo, true, false)));
+    }
+
+  } else if (target == 0x8 && enable.any()) { // Write to depth
+
+  } else if (target >= 0xc && target <= 0xf) { // Vertex output
+    auto const pos = target - 0xc;
+
+  } else if (target >= 0x20 && target <= 0x3F) { //  output params
+    auto const pos = target - 0x20;
+  }
   return true;
 }
 } // namespace compiler::frontend::translate
