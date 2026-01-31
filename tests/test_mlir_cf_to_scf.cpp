8cec6dc354fc7168366656e9e696f164d7da3355
SysRay <s.baliet@gmx.de>
2026-01-31 22:24:28 +0100

    dump example

---
+++ tests/test_mlir_cf_to_scf.cpp
@@ -0,0 +1,94 @@
+
+#include <algorithm>
+#include <cstdint>
+#include <gtest/gtest.h>
+#include <memory_resource>
+#include <ranges>
+#include <vector>
+
+// mlir
+#include <mlir/Dialect/Arith/IR/Arith.h>
+#include <mlir/Dialect/ControlFlow/IR/ControlFlowOps.h>
+#include <mlir/Dialect/Func/IR/FuncOps.h>
+#include <mlir/IR/BuiltinOps.h>
+
+class Restructure: public ::testing::Test {
+  protected:
+  void SetUp() override {
+    _mlirCtx.disableMultithreading();
+    //_mlirCtx.allowUnregisteredDialects();
+
+    _mlirCtx.loadDialect<mlir::func::FuncDialect, mlir::arith::ArithDialect, mlir::cf::ControlFlowDialect>();
+  }
+
+  void TearDown() override {}
+
+  mlir::MLIRContext _mlirCtx;
+};
+
+struct edge_t {
+  uint32_t from;
+  int32_t  to;
+  int32_t  to2 = -1;
+};
+
+static void createCFG(mlir::MLIRContext& ctx, uint32_t numBlocks, std::initializer_list<edge_t> edges) {
+  mlir::OpBuilder builder(&ctx);
+
+  auto mlirModule = mlir::ModuleOp::create(mlir::UnknownLoc::get(&ctx));
+
+  auto funcOp = builder.create<mlir::func::FuncOp>(mlir::UnknownLoc::get(&ctx), "test_func", builder.getFunctionType({builder.getI1Type()}, {}));
+
+  std::vector<mlir::Block*> blocks(numBlocks);
+  blocks[0] = funcOp.addEntryBlock();
+
+  for (uint32_t n = 1; n < blocks.size(); ++n) {
+    blocks[n] = builder.createBlock(&funcOp.getBody());
+  }
+
+  builder.setInsertionPointToEnd(blocks[0]);
+
+  for (auto const& edge: edges) {
+    if (edge.to2 != -1) {
+      builder.setInsertionPointToEnd(blocks[edge.from]);
+      builder.create<mlir::cf::CondBranchOp>(mlir::UnknownLoc::get(&ctx), funcOp.getArgument(0), blocks[edge.to], blocks[edge.to2]);
+    } else {
+      builder.setInsertionPointToEnd(blocks[edge.from]);
+      builder.create<mlir::cf::BranchOp>(mlir::UnknownLoc::get(&ctx), blocks[edge.to]);
+    }
+  }
+
+  funcOp.dump();
+}
+
+TEST_F(Restructure, SimpleIfElse) {
+  //   0
+  //   |
+  //   1
+  //  / \
+  // 2   3
+  // |   |
+  // 4   |
+  //  \ /
+  //   5
+  //   |
+  //   6
+
+  // std::pmr::monotonic_buffer_resource resource;
+  // std::pmr::polymorphic_allocator<>   allocator {&resource};
+
+  // rvsdg::IRBlocks blocks(allocator, 7);
+  // ControlFlow     cfg(allocator, blocks);
+
+  // createCFG(cfg, 7, 0, 6, {{0, 1}, {1, 2}, {1, 3}, {2, 4}, {4, 5}, {3, 5}, {5, 6}});
+  // // createCFG(cfg, 5, 0, 4, {{0, 1}, {1, 2}, {1, 4}, {2, 4}});
+  // debug::dump(std::cout, cfg);
+
+  // std::array<uint8_t, 10000>          buffer;
+  // compiler::util::checkpoint_resource tempResource(buffer.data(), buffer.size());
+  // compiler::transform::createRVSDG(tempResource, cfg);
+  // debug::dump(std::cout, blocks);
+  // EXPECT_FALSE(true); // todo
+
+  createCFG(_mlirCtx, 7, {{0, 1}, {1, 3, 2}, {2, 4}, {4, 5}, {3, 5}, {5, 6}});
+}
