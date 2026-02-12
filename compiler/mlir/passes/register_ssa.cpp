90c95565dfedcca8edaa7f0ed55bddddae602b41
SysRay <s.baliet@gmx.de>
2026-02-12 18:18:39 +0100

    + combine/split cases

---
+++ compiler/mlir/passes/register_ssa.cpp
@@ -0,0 +1,168 @@
+#include "frontend/gfx/register_types.h"
+#include "frontend/shader_types.h"
+#include "mlir/custom.h"
+#include "psoff_passes.h"
+#include "util/bump_allocator.h"
+
+#include <unordered_map>
+
+#define GEN_PASS_DEF_REGISTERSSAPASS
+#include "psOff.td.pass.h.inc"
+
+namespace mlir::psoff {
+
+static inline bool is64BitType(mlir::Type ty) {
+  return ty.getIntOrFloatBitWidth() == 64;
+}
+
+// last two SGPRs are alias to VCC.
+// However the hardware does not prevent direct user access to these registers.
+
+// Ignore out of bounds writes
+
+struct StorageItem {
+  mlir::Value value {};
+  uint8_t     index = 0;
+};
+
+struct Storage {
+  struct {
+    std::array<StorageItem, compiler::frontend::SPEC_TOTAL_SGPR> sgpr;
+    std::array<StorageItem, compiler::frontend::SPEC_TOTAL_VGPR> vgpr;
+
+    // Value exec_lo;
+    // Value exec_hi;
+    // Value m0;
+    // Value vskip;
+  };
+
+  void        set(mlir::Location loc, compiler::frontend::eOperandKind kind, mlir::Value value);
+  StorageItem get(mlir::Location loc, compiler::frontend::eOperandKind kind);
+};
+
+void Storage::set(mlir::Location loc, compiler::frontend::eOperandKind kind, mlir::Value value) {
+  using namespace compiler::frontend;
+
+  StorageItem* item = nullptr;
+  switch (kind.base()) {
+    case eOperandKind::eBase::SGPR: {
+      item = &sgpr[kind.getSGPR()];
+    } break;
+    case eOperandKind::eBase::VGPR: {
+      item = &vgpr[kind.getVGPR()];
+    } break;
+    default: {
+      emitError(loc, "Unknown kind");
+    } break;
+  }
+
+  *item++ = {value, 0};
+  if (is64BitType(value.getType())) {
+    *item = {value, 1};
+  }
+}
+
+StorageItem Storage::get(mlir::Location loc, compiler::frontend::eOperandKind kind) {
+  using namespace compiler::frontend;
+  switch (kind.base()) {
+    case eOperandKind::eBase::SGPR: {
+      return sgpr[kind.getSGPR()];
+    } break;
+    case eOperandKind::eBase::VGPR: {
+      return vgpr[kind.getVGPR()];
+    } break;
+    default: {
+      emitError(loc, "Unknown kind");
+    } break;
+  }
+
+  return {};
+}
+
+// static mlir::Value getB32(std::pair<StorageKey_t, Storage> const& value) {
+//   auto const width = value.second.value.getType().getIntOrFloatBitWidth();
+//   if (width == 32)
+//     return value.second.value;
+//   else if (width < 32)
+//     return _mlirData.create<mlir::arith::ExtUIOp>(_types._i32, value.second.value).getResult();
+
+//   auto splitValue = _mlirData.create<mlir::psoff::Split64bOp>(_types._i32, _types._i32, getB64(value));
+
+//   if (value.first.second == 0) return splitValue.getLo();
+//   return splitValue.getHi();
+// };
+
+struct RegisterSSAPass: public ::impl::RegisterSSAPassBase<RegisterSSAPass> {
+  compiler::util::BumpAllocator& _allocator;
+
+  RegisterSSAPass(compiler::util::BumpAllocator& allocator): _allocator(allocator) {}
+
+  void runOnOperation() final {
+    auto const& funcOp = Pass::getOperation();
+
+    PatternRewriter rewriter(&getContext());
+
+    std::pmr::unordered_map<Block*, Storage> values(&_allocator); // todo or use stack
+
+    auto curItem = values.emplace(funcOp->getBlock(), Storage {}).first;
+
+    using namespace compiler::frontend;
+
+    funcOp->walk([&](Block* block) {
+      for (Operation& opBase: llvm::make_early_inc_range(*block)) {
+        if (auto op = dyn_cast<psoff::StoreOp>(opBase)) {
+          auto const kind = eOperandKind((eOperandKind_t)op.getId().getZExtValue());
+          curItem->second.set(op.getLoc(), kind, op.getVal());
+        } else if (auto op = dyn_cast<psoff::LoadOp>(opBase)) {
+          auto const kind = eOperandKind((eOperandKind_t)op.getId().getZExtValue());
+          auto       item = curItem->second.get(op.getLoc(), kind);
+
+          if (!item.value) {
+            signalPassFailure();
+            return WalkResult::interrupt();
+          }
+
+          auto const targetType = op.getType();
+          auto const valueType  = item.value.getType();
+          if (targetType != valueType) { // Handle different type width
+            auto const targetWidth = targetType.getIntOrFloatBitWidth();
+            auto const valueWidth  = valueType.getIntOrFloatBitWidth();
+
+            if (targetWidth > valueWidth) {
+              // Gather lower part of 64 bit value and combine to 64 bit value
+              auto itemL = curItem->second.get(op.getLoc(), eOperandKind((eOperandKind_t)kind.value() + 1));
+              if (!itemL.value) {
+                signalPassFailure();
+                return WalkResult::interrupt();
+              }
+
+              rewriter.setInsertionPoint(op);
+              auto newOp = rewriter.replaceOpWithNewOp<mlir::psoff::Create64bOp>(op, targetType, itemL.value, item.value);
+              curItem->second.set(op.getLoc(), kind, newOp);
+            } else if (targetWidth < valueWidth) {
+              rewriter.setInsertionPoint(op);
+              auto newOp = rewriter.create<mlir::psoff::Split64bOp>(op.getLoc(), targetType, targetType, item.value);
+              rewriter.replaceOp(op, item.index == 0 ? newOp.getLo() : newOp.getHi());
+
+              curItem->second.set(op.getLoc(), kind, newOp.getLo());
+              if (item.index == 0) {
+                curItem->second.set(op.getLoc(), eOperandKind((eOperandKind_t)kind.value() + 1), newOp.getHi());
+              }
+            } else {
+              // bitcast
+            }
+          }
+
+          else
+            op.replaceAllUsesWith(item.value);
+        }
+      }
+      return WalkResult::advance();
+    });
+  }
+};
+
+std::unique_ptr<Pass> createRegisterSSAPass(compiler::util::BumpAllocator& allocator) {
+  return std::make_unique<RegisterSSAPass>(allocator);
+}
+} // namespace mlir::psoff
--- compiler/tests/register_ssa_test.mlir
+++ compiler/tests/register_ssa_test.mlir
@@ -1,15 +1,78 @@
 // RUN: mlir-opt --registerSSA --remove-dead-values %s | FileCheck %s
 
-// CHECK-LABEL: func.func @main
+// CHECK-LABEL: func.func @simple
 // CHECK-SAME: (%[[ARG0:.*]]: f32) -> i1
 // CHECK: %[[CST:.*]] = arith.constant 0.000000e+00 : f32
 // CHECK: %[[CMP:.*]] = arith.cmpf one, %[[CST]], %[[ARG0]] : f32
 // CHECK: return %[[CMP]] : i1
-
-func.func @main(%arg0: f32) -> i1 {
+func.func @simple(%arg0: f32) -> i1 {
   psoff.store s[4] = %arg0 : f32
   %cst = arith.constant 0.000000e+00 : f32
   %2 = psoff.load s[4] : f32
   %3 = arith.cmpf one, %cst, %2 : f32
   return %3 : i1
+}
+
+// CHECK-LABEL: func.func @combined
+// CHECK-SAME: (%[[ARG0:.*]]: i32, %[[ARG1:.*]]: i32) -> i64
+// CHECK:         %[[VAL0:.*]] = "psoff.create64b"(%[[ARG1]], %[[ARG0]]) : (i32, i32) -> i64
+// CHECK:         %[[VAL1:.*]] = "psoff.create64b"(%[[ARG0]], %[[ARG1]]) : (i32, i32) -> i64
+// CHECK:         %[[RES:.*]] = arith.addi %[[VAL0]], %[[VAL1]] : i64
+// CHECK:         return %[[RES]] : i64
+func.func @combined(%arg0: i32, %arg1: i32) -> i64 {
+  psoff.store s[4] = %arg0 : i32
+  psoff.store s[5] = %arg1 : i32
+  psoff.store s[6] = %arg1 : i32
+  psoff.store s[7] = %arg0 : i32
+
+  %2 = psoff.load s[4] : i64
+  %3 = psoff.load s[6] : i64
+
+  %res = arith.addi %2, %3 : i64
+  return %res : i64
+}
+
+func.func @combined64b(%arg0: i32, %arg1: i32) -> i64 {
+  psoff.store s[4] = %arg0 : i32
+  psoff.store s[5] = %arg1 : i32
+  psoff.store s[6] = %arg1 : i32
+  psoff.store s[7] = %arg0 : i32
+
+  %2 = psoff.load s[4] : i64
+  %3 = psoff.load s[6] : i64
+
+  %res = arith.addi %2, %3 : i64
+  return %res : i64
+}
+
+// CHECK-LABEL: func.func @split
+// CHECK-SAME: (%[[ARG0:.*]]: i64)
+// CHECK:   %[[LO:.*]], %[[HI:.*]] = "psoff.split64b"(%[[ARG0]]) : (i64) -> (i32, i32)
+// CHECK:   %[[VAL0:.*]] = arith.addi %[[LO]], %[[HI]] : i32
+// CHECK:   return %[[VAL0]] : i32
+func.func @split(%arg0: i64) -> i32 {
+  psoff.store s[4] = %arg0 : i64
+
+  %2 = psoff.load s[4] : i32
+  %3 = psoff.load s[5] : i32
+
+  %res = arith.addi %2, %3 : i32
+  return %res : i32
+}
+
+// CHECK-LABEL: func.func @splitUpperLower
+// CHECK-SAME: (%[[ARG0:.*]]: i64, %[[ARG1:.*]]: i64)
+// CHECK:   %[[VAL0:.*]], %[[VAL1:.*]] = "psoff.split64b"(%[[ARG0:.*]]) : (i64) -> (i32, i32)
+// CHECK:   %[[VAL2:.*]], %[[VAL3:.*]] = "psoff.split64b"(%[[ARG1:.*]]) : (i64) -> (i32, i32)
+// CHECK:   %[[VAL4:.*]] = arith.addi %[[VAL1]], %[[VAL2]] : i32
+// CHECK:   return %[[VAL4]] : i32
+func.func @splitUpperLower(%arg0: i64, %arg1: i64) -> i32 {
+  psoff.store s[4] = %arg0 : i64
+  psoff.store s[6] = %arg1 : i64
+
+  %2 = psoff.load s[5] : i32
+  %3 = psoff.load s[6] : i32
+
+  %res = arith.addi %2, %3 : i32
+  return %res : i32
 }
